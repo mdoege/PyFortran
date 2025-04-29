16c441325c9575514cf37a435d5da0656b485c81
Martin C. Doege <mdoege@compuserve.com>
2025-04-29 15:37:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## PyFortran
+
+A Python interpreter for early FORTRAN, i.e. to run programs written for FORTRAN on the IBM 704 and FORTRAN / GOTRAN for the IBM 1620 or FORTRANSIT on the IBM 650
+
+Usage:
+
+  `python pyfortran.py [source file]`
+
+### Differences between PyFortran and historical FORTRAN languages
+
+* FORMAT statements are ignored.
+* no arrays
+* no continuation lines
+* Source code can be in lowercase.
+* Integer division works like in Python (e.g. 3/2 == 1.5 in Python 3, unlike Python 2 or FORTRAN).
+* Completely empty, non-comment lines are allowed.
+* STOP or END are optional.
+* Jumping out of loops probably causes problems when later entering same loop.
+* Loops can use real numbers, not just integers. (But the loop variable can only increase, as in old FORTRAN.)
+
---
+++ pyfortran.py
@@ -0,0 +1,149 @@
+# Fortran interpreter
+
+import sys
+from math import *
+
+fn = sys.argv[1]
+
+src = open(fn).readlines()
+src = [q.upper() for q in src]
+cur = 0
+
+lab = {}    # labels
+var = {}    # variables
+loops = {}  # loops
+
+# built-in functions
+funcs = ("COSF", "INTF", "SQRTF")
+
+def COSF(x):
+    return cos(x)
+
+def INTF(x):
+    return int(x)
+
+def SQRTF(x):
+    return sqrt(x)
+
+def beg(cmd):
+    # check for Fortran keyword
+    if src[cur][6:6+len(cmd)] == cmd:
+        return True
+    else:
+        return False
+
+def repvar(l):
+    # replace variable names with contents
+    l += "?"
+    o = ""
+    v = ""
+    for x in l:
+        if x.isalpha() and len(v) == 0:
+            v += x
+        elif x.isalnum() and len(v) > 0:
+            v += x
+        else:
+            if len(v):
+                if v in funcs:
+                    o += v
+                else:
+                    o += str(var[v])
+                v = ""
+            o += x
+    return o[:-1]
+
+# check for all labels
+for l in range(len(src)):
+    curlab = src[l][:5].strip()
+    if curlab != "":
+        lab[curlab] = l
+
+while True:
+    if src[cur][0] == "C":  # skip comment lines
+        #print(src[cur].strip())
+        cur += 1
+        continue
+
+    #print(cur, src[cur])
+
+    # check for current label
+    curlab = src[cur][:5].strip()
+    if curlab != "":
+        lab[curlab] = cur
+
+    if beg("STOP") or beg("END"):
+        break
+
+    line = src[cur][6:72].replace(" ", "").strip()
+
+    # assignment
+    if "=" in line and not beg("DO"):
+        left, right = line.split("=")
+        right = repvar(right)
+        var[left] = eval(right)
+        #print(var[left])
+
+    if beg("PRINT"):
+        print(repvar(line[5:]))
+
+    if beg("PUNCH"):
+        v = line[5:].split(",")
+        for x in v:
+            if len(x) > 0:
+                print("%12u" % var[x], end = "")
+        print()
+
+    if beg("DO"):
+        left, right = line.split("=")
+        right = repvar(right)
+        lim = eval(right)
+        va = "".join([q for q in left if q.isalpha()])[2:]
+        la = "".join([q for q in left if not q.isalpha()])
+        var[va] = lim[0]
+        if len(lim) > 2:
+            inc = lim[2]
+        else:
+            inc = 1
+        newloop = loops.get(la, [])
+        newloop.append((va, lim[1], inc, cur))
+        loops[la] = newloop
+        #print(va, la, inc)
+
+    if beg("GO TO") or beg("GOTO"):
+        cur = lab[line[4:].strip()]
+        continue
+
+    if beg("IF"):
+        par = line.find(")")
+        test = line[3:par]
+        targ = line[par+1:].split(",")
+        v = eval(repvar(test))
+        if v < 0:
+            cur = lab[targ[0].strip()]
+        elif v > 0:
+            cur = lab[targ[2].strip()]
+        else:
+            cur = lab[targ[1].strip()]
+        continue
+
+    if beg("PAUSE"):
+        print("*** program paused; press Return to continue")
+        input()
+
+    # check if on final line of a loop
+    if curlab in loops.keys() and len(loops[curlab]) > 0:
+        #print(loops[curlab])
+        while len(loops[curlab]) > 0:
+            va, lim, inc, lnum = loops[curlab][-1]
+            var[va] += inc
+            if var[va] + inc > lim: # final interation?
+                loops[curlab].pop()
+            if var[va] <= lim:
+                cur = lnum + 1
+                break
+    else:   # advance to next line
+        cur += 1
+        if cur >= len(src): # check if end of file
+            break
+
+
